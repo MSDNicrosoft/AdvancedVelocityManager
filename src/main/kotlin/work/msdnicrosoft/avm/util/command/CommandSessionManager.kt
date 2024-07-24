8813ed63f55db2237aeb5b760e9bf2976d3e507b
MSDNicrosoft <wang3311835119@hotmail.com>
2024-07-25 02:15:12 +0800

    feat(command-session-manager): Implement `Command Session Manager` to support temporary commands
    Signed-off-by: MSDNicrosoft <wang3311835119@hotmail.com>

---
+++ src/main/kotlin/work/msdnicrosoft/avm/util/command/CommandSessionManager.kt
@@ -0,0 +1,154 @@
+package work.msdnicrosoft.avm.util.command
+
+import taboolib.common.platform.Platform
+import taboolib.common.platform.PlatformSide
+import taboolib.common.platform.function.submitAsync
+import taboolib.common.platform.function.warning
+import taboolib.common.platform.service.PlatformExecutor
+import java.security.MessageDigest
+import java.util.concurrent.ConcurrentHashMap
+
+typealias ExecuteBlock = () -> Any?
+typealias SessionId = String
+
+/**
+ * A manager for command sessions.
+ *
+ * Command sessions are used to execute commands.
+ * This class provides functionality to add, execute, and remove command sessions.
+ */
+@PlatformSide(Platform.VELOCITY)
+object CommandSessionManager {
+
+    /**
+     * The result of executing a command session.
+     */
+    enum class ExecuteResult {
+        /**
+         * The command session was executed successfully.
+         */
+        SUCCESS,
+
+        /**
+         * The command session has expired.
+         */
+        EXPIRED,
+
+        /**
+         * An error occurred while executing the command session.
+         */
+        FAILED,
+
+        /**
+         * The command session was not found.
+         */
+        NOT_FOUND
+    }
+
+    /**
+     * Represents an action to be executed in a command session.
+     *
+     * @property executed Whether the action has been executed.
+     * @property block The block of code to be executed.
+     * @property expirationTime The time at which the action expires.
+     */
+    data class Action(var executed: Boolean = false, val block: ExecuteBlock, val expirationTime: Long) {
+
+        /**
+         * @property isExpired True if the action has expired, false otherwise.
+         */
+        val isExpired: Boolean
+            get() = System.currentTimeMillis() > expirationTime
+    }
+
+    private val sessions = ConcurrentHashMap<SessionId, Action>()
+
+    /**
+     * The task responsible for removing expired command sessions.
+     */
+    private lateinit var removalTask: PlatformExecutor.PlatformTask
+
+    /**
+     * Initializes the command session manager.
+     *
+     * This function starts a task that removes expired command sessions every 20 minutes.
+     */
+    fun onEnable() {
+        removalTask = submitAsync(period = 20 * 60L) {
+            sessions.forEach { (sessionId, action) ->
+                if (action.isExpired) remove(sessionId)
+            }
+        }
+    }
+
+    /**
+     * Disables the command session manager.
+     *
+     * This function cancels the removal task.
+     */
+    fun onDisable() {
+        removalTask.cancel()
+    }
+
+    /**
+     * Adds a command session to the manager.
+     *
+     * @param sessionId The ID of the command session.
+     * @param block The block of code to be executed.
+     */
+    fun add(sessionId: SessionId, block: ExecuteBlock) {
+        sessions[sessionId] = Action(block = block, expirationTime = System.currentTimeMillis() + 60_000L)
+    }
+
+    /**
+     * Executes a command session.
+     *
+     * @param sessionId The ID of the command session.
+     * @return The result of executing the command session.
+     */
+    fun executeAction(sessionId: SessionId): ExecuteResult {
+        val session = sessions.remove(sessionId) ?: return ExecuteResult.NOT_FOUND
+        return session.run {
+            try {
+                if (isExpired) {
+                    ExecuteResult.EXPIRED
+                } else {
+                    block.invoke()
+                    ExecuteResult.SUCCESS
+                }
+            } catch (e: Exception) {
+                warning("An error occurred when executing session command: ${e.message}")
+                ExecuteResult.FAILED
+            }
+        }
+    }
+
+    /**
+     * Removes a command session from the manager.
+     *
+     * @param sessionId The ID of the command session.
+     */
+    fun remove(sessionId: SessionId) = sessions.remove(sessionId)
+
+    /**
+     * Generates a session ID based on the given parameters.
+     *
+     * @param name The name used to generate the session ID.
+     * @param time The time used to generate the session ID.
+     * @param command The command used to generate the session ID.
+     * @return The generated session ID.
+     */
+    fun generateSessionId(name: String, time: Long, command: String): String {
+        val hash = MessageDigest.getInstance("SHA-256")
+        hash.update("$name$time$command".toByteArray())
+        return buildString {
+            hash.digest().forEach { byte ->
+                val hex = Integer.toHexString(byte.toInt() and 0xFF)
+                if (hex.length == 1) {
+                    append('0')
+                }
+                append(hex)
+            }
+        }
+    }
+}
