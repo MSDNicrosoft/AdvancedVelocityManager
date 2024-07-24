42289cad7590cb242316137dfa2aa8fdda4197b6
MSDNicrosoft <wang3311835119@hotmail.com>
2024-07-25 02:10:33 +0800

    refactor: Plugin main class
    Signed-off-by: MSDNicrosoft <wang3311835119@hotmail.com>

---
+++ src/main/kotlin/work/msdnicrosoft/avm/AdvancedVelocityManagerPlugin.kt
@@ -0,0 +1,110 @@
+package work.msdnicrosoft.avm
+
+import taboolib.common.platform.Platform
+import taboolib.common.platform.PlatformFactory
+import taboolib.common.platform.PlatformSide
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.getDataFolder
+import taboolib.common.platform.function.info
+import taboolib.common.platform.function.warning
+import taboolib.common.util.unsafeLazy
+import taboolib.module.lang.Language
+import taboolib.platform.VelocityPlugin
+import work.msdnicrosoft.avm.config.AVMConfig
+import work.msdnicrosoft.avm.impl.VelocityAdapter
+import work.msdnicrosoft.avm.module.chatbridge.ChatBridge
+import work.msdnicrosoft.avm.module.chatbridge.ChatBridge.PassthroughMode
+import work.msdnicrosoft.avm.module.whitelist.PlayerCache
+import work.msdnicrosoft.avm.module.whitelist.WhitelistManager
+import work.msdnicrosoft.avm.util.ConfigUtil.yaml
+import work.msdnicrosoft.avm.util.Extensions.decodeFromString
+import work.msdnicrosoft.avm.util.Extensions.encodeToString
+import work.msdnicrosoft.avm.util.command.CommandSessionManager
+
+@PlatformSide(Platform.VELOCITY)
+object AdvancedVelocityManagerPlugin : Plugin() {
+
+    val plugin by unsafeLazy { VelocityPlugin.getInstance() }
+
+    val configFile by unsafeLazy { getDataFolder().resolve("config.yml") }
+
+    val configLock = Object()
+
+    lateinit var config: AVMConfig
+
+    var hasFloodgate: Boolean = false
+
+    override fun onLoad() {
+        val adapter = VelocityAdapter()
+        val adapterKey = PlatformFactory.serviceMap.keys.first { it.contains("PlatformAdapter") }
+        PlatformFactory.serviceMap[adapterKey] = adapter
+    }
+
+    override fun onEnable() {
+        hasFloodgate = plugin.server.pluginManager.getPlugin("floodgate") != null
+        plugin.logger.debug("Nya~!")
+        loadConfig()
+        Language.default = config.defaultLang
+        CommandSessionManager.onEnable()
+        PlayerCache.onEnable()
+        WhitelistManager.onEnable()
+    }
+
+    override fun onDisable() {
+        WhitelistManager.onDisable()
+        CommandSessionManager.onDisable()
+    }
+
+    private fun loadConfig(reload: Boolean = false) {
+        if (configFile.exists()) {
+            try {
+                info("${if (reload) "Reloading" else "Loading"} config...")
+                withLock { config = yaml.decodeFromString<AVMConfig>(configFile.readText()) }
+                checkConfig()
+            } catch (e: Exception) {
+                error("Failed to load config: ${e.message}")
+            }
+        } else {
+            try {
+                info("Config file does not exist, generating default config...")
+                configFile.parentFile.mkdirs()
+                withLock {
+                    config = AVMConfig()
+                    configFile.writeText(yaml.encodeToString(config))
+                }
+            } catch (e: Exception) {
+                error("Failed to initialize config: ${e.message}")
+            }
+        }
+    }
+
+    fun saveConfig() = withLock {
+        runCatching {
+            configFile.writeText(yaml.encodeToString(config))
+        }.onFailure {
+            error("Failed to save config: ${it.message}")
+        }.isSuccess
+    }
+
+    private fun checkConfig() {
+        try {
+            ChatBridge.mode = PassthroughMode.valueOf(config.chatBridge.chatPassthrough.mode.uppercase())
+        } catch (_: IllegalArgumentException) {
+            warning("Incorrect Chat-Passthrough mode name!")
+            warning("Plugin will fallback to `ALL` mode")
+            ChatBridge.mode = PassthroughMode.ALL
+        }
+    }
+
+    fun <T> withLock(block: () -> T) = synchronized(configLock) { block() }
+
+    fun reload() = runCatching {
+        loadConfig(reload = true)
+
+        info("Reloading language files...")
+        Language.reload()
+
+        WhitelistManager.onEnable(reload = true)
+        PlayerCache.onEnable()
+    }.isSuccess
+}
