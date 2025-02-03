98f21c4660091868f7734d2fb2902d41a739ac7f
MSDNicrosoft <wang3311835119@hotmail.com>
2025-02-03 23:10:41 +0800

    fix(msg-command): Incomplete player completion
    - Optimize code
    
    Signed-off-by: MSDNicrosoft <wang3311835119@hotmail.com>

--- src/main/kotlin/work/msdnicrosoft/avm/command/chatbridge/MsgCommand.kt
+++ src/main/kotlin/work/msdnicrosoft/avm/command/chatbridge/MsgCommand.kt
@@ -10,17 +10,17 @@ import taboolib.common.platform.command.CommandBody
 import taboolib.common.platform.command.CommandHeader
 import taboolib.common.platform.command.PermissionDefault
 import taboolib.common.platform.command.mainCommand
-import taboolib.common.platform.command.player
 import taboolib.common.util.isConsole
+import taboolib.common.util.presentRun
 import taboolib.module.lang.sendLang
 import work.msdnicrosoft.avm.config.ConfigManager
 import work.msdnicrosoft.avm.util.DateTimeUtil.getDateTime
+import work.msdnicrosoft.avm.util.ProxyServerUtil.getPlayer
 import work.msdnicrosoft.avm.util.component.ComponentUtil.createClickEvent
 import work.msdnicrosoft.avm.util.component.ComponentUtil.createHoverEvent
 import work.msdnicrosoft.avm.util.component.ComponentUtil.serializer
 import work.msdnicrosoft.avm.util.component.Format
 import kotlin.jvm.optionals.getOrElse
-import kotlin.jvm.optionals.getOrNull
 import work.msdnicrosoft.avm.AdvancedVelocityManagerPlugin as AVM
 
 @PlatformSide(Platform.VELOCITY)
@@ -32,56 +32,63 @@ object MsgCommand {
 
     @CommandBody
     val main = mainCommand {
-        player("targets") {
+        dynamic("targets") {
             suggestion<ProxyCommandSender>(uncheck = false) { sender, _ ->
-                if (config.takeOverPrivateChat) {
+                if (config.takeOverPrivateChat || sender.isConsole()) {
                     AVM.plugin.server.allPlayers.map { it.username }
                 } else {
-                    AVM.plugin.server.getPlayer(sender.name).getOrNull()
-                        ?.currentServer?.getOrNull()
-                        ?.server?.playersConnected?.map { it.username }
+                    getPlayer(sender.name).get()
+                        .currentServer.get()
+                        .server.playersConnected.map { it.username }
                 }
             }
             dynamic("message") {
                 execute<ProxyCommandSender> { sender, context, _ ->
-                    val targets = context.player("targets")
-                    val player = AVM.plugin.server.getPlayer(targets.name).getOrElse {
-                        sender.sendLang("player-not-found", targets.name)
+                    val targets = context["targets"]
+                    val player = getPlayer(targets).getOrElse {
+                        sender.sendLang("player-not-found", targets)
                         return@execute
                     }
 
+                    val message = context["message"]
+
                     if (!sender.isConsole()) {
-                        AVM.plugin.server.getPlayer(sender.name).get().sendMessage(
-                            config.privateChatFormat.sender.buildMessage(sender, player, context["message"])
-                        )
+                        getPlayer(sender.name).presentRun {
+                            sendMessage(
+                                config.privateChatFormat.sender.buildMessage(sender, player, message)
+                            )
+                        }
                     }
                     player.sendMessage(
-                        config.privateChatFormat.receiver.buildMessage(sender, player, context["message"])
+                        config.privateChatFormat.receiver.buildMessage(sender, player, message)
                     )
                 }
             }
         }
     }
 
-    private fun List<Format>.buildMessage(sender: ProxyCommandSender, player: Player, message: String) =
-        Component.join(
+    private fun List<Format>.buildMessage(sender: ProxyCommandSender, player: Player, message: String): Component {
+        val time = getDateTime()
+        return Component.join(
             JoinConfiguration.noSeparators(),
             this.map { format ->
-                format.text.deserialize(sender.name, player.username, message)
-                    .hoverEvent(createHoverEvent(format) { deserialize(sender.name, player.username, message) })
-                    .clickEvent(createClickEvent(format) { replacePlaceHolders(sender.name, player.username) })
+                format.text.deserialize(sender.name, player.username, message, time)
+                    .hoverEvent(createHoverEvent(format) { deserialize(sender.name, player.username, message, time) })
+                    .clickEvent(createClickEvent(format) { replacePlaceHolders(sender.name, player.username, time) })
             }
         )
+    }
 
-    private fun String.deserialize(from: String, to: String, message: String) = serializer.buildComponent(this)
+    private fun String.deserialize(from: String, to: String, message: String, dateTime: String): Component =
+        serializer.buildComponent(this)
+            .replace("%player_name_from%", from)
+            .replace("%player_name_to%", to)
+            .replace("%player_message%", message.let { if (config.allowFormatCode) SERIALIZER.parse(it) else it })
+            .replace("%player_message_sent_time%", dateTime)
+            .build()
+
+    private fun String.replacePlaceHolders(from: String, to: String, dateTime: String): String = this
         .replace("%player_name_from%", from)
         .replace("%player_name_to%", to)
-        .replace("%player_message%", message.let { if (config.allowFormatCode) serializer.parse(it) else it })
-        .replace("%player_message_sent_time%", getDateTime())
-        .build()
-
-    private fun String.replacePlaceHolders(from: String, to: String) = this
-        .replace("%player_name_from%", from)
-        .replace("%player_name_to%", to)
-        .replace("%player_message_sent_time%", getDateTime())
+        .replace("%player_message_sent_time%", dateTime)
 }
