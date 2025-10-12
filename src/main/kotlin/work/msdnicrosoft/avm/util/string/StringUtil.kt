1d9721b1e4472475926a35fb779981d683b4060e
MSDNicrosoft <i@msdnicrosoft.work>
2025-10-12 19:45:17 +0800

    fix: Support HTTP URL in custom API endpoint
    Signed-off-by: MSDNicrosoft <i@msdnicrosoft.work>

--- src/main/kotlin/work/msdnicrosoft/avm/util/string/StringUtil.kt
+++ src/main/kotlin/work/msdnicrosoft/avm/util/string/StringUtil.kt
@@ -1,5 +1,5 @@
 package work.msdnicrosoft.avm.util.string
 
 object StringUtil {
-    val URL_PATTERN = Regex("^(https?)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")
+    val URL_PATTERN = Regex("^(https?|http?)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")
 }
